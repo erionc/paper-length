fa9ba537d48fd44343d570309ccab0a77fb9af98
erionc <gh.user.one@gmail.com>
2020-06-28 22:00:13 +0200

    finalized set2

--- set2_dense.py
+++ set2_dense.py
@@ -56,7 +56,8 @@ def summary_from_keywords(key_list):
 
 # function that tokenizes text same as Stanford CoreNLP
 def core_tokenize(text):
-	''' Takes a text string and returns tokenized string using NLTK word_tokenize 
+	''' 
+	Takes a text string and returns tokenized string using NLTK word_tokenize 
 	same as in Stanford CoreNLP. space, \n \t are lost. "" are replace by ``''
 	'''
 	# tokenize | _ ^ / ~ + = * that are not tokenized by word_tokenize
@@ -85,17 +86,15 @@ def core_tokenize(text):
 	text = ' '.join(tokens)
 	# remove double+ spaces
 	text = re.sub(r'\s{2,}', " ", text)
-
 	# lowercase
 	text = text.lower()
 	# remove special characters by performing encode-decode in ascii
 	text = text.encode('ascii', 'ignore').decode('ascii')
-
 	return text
 
-# tokenizes text in ["abstract", "title"] fields of a dictionary or dataframe record
+# tokenizes text in ["abstract", "title", "keywords"] fields
 def record_tokenize(rec):
-	''' Tokenizes ALL fields of a dictionary or dataframe record wich core_tokenize'''
+	''' Tokenizes ALL fields of a dictionary or dataframe record with core_tokenize'''
 	for k,v in rec.items():
 		if k in ["abstract", "title"]:
 			rec[k] = core_tokenize(v)
@@ -107,7 +106,7 @@ def record_tokenize(rec):
 # read file json lines from given file path and return them in a list
 def read_dicts_from_list(file_path):
 	'''read json lines and store them in a list that is returned'''
-	with open(file_path, "r", encoding = 'utf-8') as inf:   
+	with open(file_path, "r", encoding='utf-8') as inf:   
 		# strip \n at the end of each line
 		line_list = [json.loads(line) for line in inf]
 	return line_list
@@ -115,53 +114,11 @@ def read_dicts_from_list(file_path):
 # write list records as lines in a given file path
 def write_dicts_to_file(file_path, line_list):
 	'''write list lines in a file path that is opened'''
-	outf = open(file_path, "a", encoding = 'utf-8')  # in this case i need to append to file
+	outf = open(file_path, "a", encoding='utf-8')  
 	for itm in line_list:
 		json.dump(itm, outf)
 		outf.write('\n')
 	outf.close()
-
-def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
-	# Clean the text, with the option to remove stopwords and to stem words.
-	
-	# Convert words to lower case and split them
-	text = text.lower().split()
-  
-	text = " ".join(text)
-
-	# # Clean the text
-	# text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
-	# text = re.sub(r"what's", "what is ", text)
-	# text = re.sub(r"\'s", " ", text)
-	# text = re.sub(r"\'ve", " have ", text)
-	# text = re.sub(r"can't", "cannot ", text)
-	# text = re.sub(r"n't", " not ", text)
-	# text = re.sub(r"i'm", "i am ", text)
-	# text = re.sub(r"\'re", " are ", text)
-	# text = re.sub(r"\'d", " would ", text)
-	# text = re.sub(r"\'ll", " will ", text)
-	# text = re.sub(r",", " ", text)
-	# text = re.sub(r"\.", " ", text)
-	# text = re.sub(r"!", " ! ", text)
-	# text = re.sub(r"\/", " ", text)
-	# text = re.sub(r"\^", " ^ ", text)
-	# text = re.sub(r"\+", " + ", text)
-	# text = re.sub(r"\-", " - ", text)
-	# text = re.sub(r"\=", " = ", text)
-	# text = re.sub(r"'", " ", text)
-	# text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
-	# text = re.sub(r":", " : ", text)
-	# text = re.sub(r" e g ", " eg ", text)
-	# text = re.sub(r" b g ", " bg ", text)
-	# text = re.sub(r" u s ", " american ", text)
-	# text = re.sub(r"\0s", "0", text)
-	# text = re.sub(r" 9 11 ", "911", text)
-	# text = re.sub(r"e - mail", "email", text)
-	# text = re.sub(r"j k", "jk", text)
-	# text = re.sub(r"\s{2,}", " ", text)
-	
-	# Return a list of words
-	return(text)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--embeddings', choices=['gs', 'gb', 'w2v'], help='Embedding type', required=True)
